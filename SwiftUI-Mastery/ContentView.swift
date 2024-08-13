7243b28f20353025cb3675ae8c848f4d0d8469c7
Ayaz Rafai <ayazrafai90@gmail.com>
2024-08-13 21:14:35 +0530

    Initial XCode project setup

---
+++ SwiftUI-Mastery/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-Mastery
+//
+//  Created by Ayaz Rafai on 13/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-Mastery/SwiftUI_MasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MasteryApp.swift
+//  SwiftUI-Mastery
+//
+//  Created by Ayaz Rafai on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
