c45ce42a72137b28da128ce9a272631054aec63b
Ayaz Rafai <ayazrafai90@gmail.com>
2024-08-18 22:16:21 +0530

    Alert & ActionSheet: Explore the basic aspects of the `Alert` & `ActionSheet`

--- SwiftUI-Mastery/AlertPractice.swift
+++ SwiftUI-Mastery/AlertPractice.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct AlertPractice: View {
     
     @State var showAlert = false
+    @State var showActionSheet = false
     
     var body: some View {
         ZStack {
@@ -18,21 +19,69 @@ struct AlertPractice: View {
             VStack {
                 TitleView(description: "Explore basic integration for the alert view", color: .white, descriptionColor: .white.opacity(0.5))
                 Spacer()
-                Button("Show alert") {
-                    showAlert.toggle()
+                
+                
+                VStack {
+                    HStack {
+                        Image(systemName: "person.fill")
+                            .foregroundStyle(.black)
+                            .frame(width: 34, height: 34)
+                            .background(Circle().fill(.gray))
+                        Text("Ayaz Rafai")
+                        Spacer()
+                        
+                        Button {
+                            showAlert = true
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .resizable()
+                                .scaledToFit()
+                                .padding(.horizontal, 4)
+                                .frame(width: 34, height: 34)
+                                .foregroundStyle(.customPink)
+                        }
+                        .frame(width: 34, height: 34)
+                    }.foregroundStyle(.black)
+                        .zIndex(1)
+                    Image("steve")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(height: 200)
+                        .frame(maxWidth: .infinity)
+                        .clipped()
+                        .clipShape(RoundedRectangle(cornerRadius: 8.0))
+                }
+                .padding()
+                .background(.white)
+                VStack {
+                    Button("Show alert") {
+                        showAlert.toggle()
+                    }
+                    .frame(height: 60)
+                    .padding(.horizontal, 22)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(.white))
+                    
+                    
+                    Button("Show Sheet") {
+                        showActionSheet.toggle()
+                    }
+                    .frame(height: 60)
+                    .padding(.horizontal, 22)
+                    .background(RoundedRectangle(cornerRadius: 8).fill(.white))
                 }
                 .tint(.customPink)
                 .font(.headline)
-                .frame(height: 60)
-                .padding(.horizontal, 22)
-                .background(RoundedRectangle(cornerRadius: 8).fill(.white))
                 .shadow(color: .white, radius: 5, x: 0, y: 0)
-                .alert(isPresented: $showAlert, content: {
-                    //Alert(title: Text("SwiftUI-Mastery"))
-                    getAlert(title: "SwiftUI-Mastery", message: "Are you sure you want to delete?")
-                })
                 Spacer()
             }
+            .alert(isPresented: $showAlert, content: {
+                getAlert(title: "SwiftUI-Mastery",
+                         message: "Are you sure you want to delete?")
+            })
+            .actionSheet(isPresented: $showActionSheet, content: {
+                getActionSheet(title: "SwiftUI-Mastery",
+                               message: "Are you sure you want to remove?")
+            })
         }
     }
     
@@ -41,6 +90,16 @@ struct AlertPractice: View {
                      message: Text(message),
                      primaryButton: .destructive(Text("DELETE")),
                      secondaryButton: .cancel())
+    }
+    
+    func getActionSheet(title: String, message: String) -> ActionSheet {
+        
+        let removeButton: ActionSheet.Button = .destructive(Text("Remove"))
+        let cancelButton: ActionSheet.Button = .cancel()
+        
+        return ActionSheet(title: Text(title),
+                           message: Text(message),
+                           buttons: [cancelButton, removeButton])
     }
 }
 
